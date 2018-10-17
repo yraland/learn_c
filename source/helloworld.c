6c40acf37aeb3a91e9852cad80c7bdc85db2d4fe
yraland <yiran.zhu@hotmail.com>
2018-10-17 08:13:02 -0700

    add gitignore, add helloworld

---
+++ source/helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	// git clone xxxxxxxxxx.git:下载远程代码库（github）到本地 
+	// git status: 看状态
+	// git log: 是看commit的记录， 相当于看保存的记录
+	// git add xxx : 追踪修改
+	// git add -u : 追踪所有修改的文件， 不包括新建的
+	// git add . :追踪所有没有被追踪的文件
+	// git commit -m "保存的描述" : 提交（保存）追踪的文件， 并记录为啥保存
+	// git push: 上传到远程的代码库 （ github）
+	
+	printf ("helloworld");
+	return 0;
+}
